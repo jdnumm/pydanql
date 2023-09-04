a4417fffb80e6ab7b68fc7b4253fb9c9ce94cf20
Johannes Daniel Nuemm <daniel.nuemm@blacktre.es>
2023-09-04 11:13:54 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='pydanql',
-    version='0.22',
+    version='0.23-alpha',
     packages=find_packages(),
     install_requires=[
         'psycopg2-binary',
